67a00ca2d2e5a5bf834cc1502a53c180d017987c
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-02-18 14:57:40 +0600

    done init

---
+++ src/app/compnents/NavBar.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../compnents/ui/navbar-menu";
+import { cn } from "../utils/cn";
+
+const NavBar = () => {
+    return (
+        <div>NavBar</div>
+    )
+}
+
+export default NavBar
